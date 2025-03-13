3feef977e07223d4930be442e1ffe92334e6174c
Raja Dhoundiyal <rajadhoundiyalpda5@gmail.com>
2025-03-13 18:15:35 +0530

    responsive done

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-new Splide(".splide").mount();
+document.addEventListener("DOMContentLoaded", () => {
+    const menuIcon = document.querySelector(".menu-icon");
+    const closeIcon = document.querySelector(".close-icon");
+    const nav = document.querySelector(".nav");
+  
+    menuIcon.addEventListener("click", () => {
+      nav.classList.add("active");
+      menuIcon.style.display = "none";
+      closeIcon.style.display = "block";
+    });
+  
+    closeIcon.addEventListener("click", () => {
+      nav.classList.remove("active");
+      menuIcon.style.display = "block";
+      closeIcon.style.display = "none";
+    });
+  });
+  
+
+
+
+new Splide(".splide", {
+  arrow:(boolean = false),
+}).mount();
 
